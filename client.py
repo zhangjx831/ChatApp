d652e539108eff55efd5a91e21b388f75ed2b58b
zjxgcp <jz3313@csee4119-instance-1.c.csee4119-jz3313.internal>
2022-03-21 01:12:45 +0000

    client-server

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 from socket import *
 import sys
 
-serverName, serverIP, serverPort, clientPort = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+serverName, serverIP, serverPort, clientPort = sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4])
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 info = (serverName, serverIP, serverPort)
+clientSocket.bind(('', clientPort))
 print(info, clientPort)
-clientSocket.connect((serverIP, serverPort))
+message = 'test'
+clientSocket.sendto(message.encode(), (serverIP, serverPort))
 
